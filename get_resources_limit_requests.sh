969fc651075692458f71b2276abd96c3851786e1
Ubiratan <r14.ubiratan.junior@gmail.com>
2022-04-27 15:26:17 -0300

    Adicionando deployment config ao caminho

--- get_resources_limit_requests.sh
+++ get_resources_limit_requests.sh
@@ -6,9 +6,9 @@ function get_resource_limits(){
     local namespace="$2"
 
     cpu_limits=$(oc get dc/$dc -n $namespace -o jsonpath='{.spec.strategy.resources.limits.cpu}')
-    echo "export CPU_LIMITS=${cpu_limits}" > ./${namespace}/resource_vars
+    echo "export CPU_LIMITS=${cpu_limits}" > ./${namespace}/${dc}/resource_vars
     memory_limits=$(oc get dc/$dc -n $namespace -o jsonpath='{.spec.strategy.resources.limits.memory}')
-    echo "export MEMORY_LIMITS=${memory_limits}" >> ./${namespace}/resource_vars
+    echo "export MEMORY_LIMITS=${memory_limits}" >> ./${namespace}/${dc}/resource_vars
 }
 
 function get_resource_requests(){
@@ -16,9 +16,9 @@ function get_resource_requests(){
     local namespace="$2"
 
     cpu_requests=$(oc get dc/$dc -n $namespace -o jsonpath='{.spec.strategy.resources.requests.cpu}')
-    echo "export CPU_REQUESTS=${cpu_requests}" >> ./${namespace}/resource_vars
+    echo "export CPU_REQUESTS=${cpu_requests}" >> ./${namespace}/${dc}/resource_vars
     memory_requests=$(oc get dc/$dc -n $namespace -o jsonpath='{.spec.strategy.resources.requests.memory}')
-    echo "export MEMORY_REQUESTS=${memory_requests}" >> ./${namespace}resource_vars
+    echo "export MEMORY_REQUESTS=${memory_requests}" >> ./${namespace}/${dc}/resource_vars
 }
 
 function main(){
