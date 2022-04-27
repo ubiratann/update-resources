101d39bb63eec50e4bac1d759b32524b96caa06c
Ubiratan <r14.ubiratan.junior@gmail.com>
2022-04-27 15:16:22 -0300

    Incrementando a estrutura do arquivo de variáveis

--- update_resources.sh
+++ update_resources.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
-source resource_vars
+function set_vars(){
+    local namespace="$1"
+    source $namespace/resource_vars
+}
+
+function unset_vars(){
+    unset CPU_LIMITS
+    unset CPU_REQUESTS
+    unset MEMORY_LIMITS
+    unset MEMORY_REQUESTS
+}
+
+function update_resources(){
+    local deployment_config="$1"
+    local namespace="$2"
+    oc set resources dc/$deployment_config -n $namespace --limits=cpu=$CPU_LIMITS,memory=$MEMORY_LIMITS --requests=cpu=$CPU_REQUESTS,memory=$MEMORY_REQUESTS
+}
 
 function main(){
-    local target="$1"
-    oc set resources dc/$target --limits=cpu=$CPU_LIMITS,memory=$MEMORY_LIMITS --requests=cpu=$CPU_REQUESTS,memory=$MEMORY_REQUESTS
+
+
+    local read_file=$1
+    while IFS= read -r line || [ -n "$line" ]
+    do
+        target_dc=$(echo $line |  awk -F  ":" '{print $1}')
+        source_namespace=$(echo $line |  awk -F  ":" '{print $2}')
+        target_namespace=$(echo $line |  awk -F  ":" '{print $3}')
+
+        set_vars "$source_namespace"
+        update_resources "$target_dc" "$target_namespace"
+        unset_vars
+
+    done < "$read_file"
 }
 
 main "$1"
