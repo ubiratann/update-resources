0935244dc15ec5d25473755a6c78d39be70ba7d9
Ubiratan <r14.ubiratan.junior@gmail.com>
2022-04-29 16:28:46 -0300

    Corringindo caminho jsonpath

--- get_resources_limit_requests.sh
+++ get_resources_limit_requests.sh
@@ -5,10 +5,10 @@ function get_resource_limits(){
     local deployment_config="$1"
     local namespace="$2"
 
-    cpu_limits=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.strategy.resources.limits.cpu}')
+    cpu_limits=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.temaplete.spec.containers[].resources.limits.cpu}')
     echo "export CPU_LIMITS=${cpu_limits}" > ./${namespace}/${deployment_config}/resource_vars
     
-    memory_limits=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.strategy.resources.limits.memory}')
+    memory_limits=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.temaplete.spec.containers[].resources.limits.memory}')
     echo "export MEMORY_LIMITS=${memory_limits}" >> ./${namespace}/${deployment_config}/resource_vars
     
 }
@@ -17,10 +17,10 @@ function get_resource_requests(){
     local deployment_config=$1
     local namespace="$2"
 
-    cpu_requests=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.strategy.resources.requests.cpu}')
+    cpu_requests=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.temaplete.spec.containers[].resources.requests.cpu}')
     echo "export CPU_REQUESTS=${cpu_requests}" >> ./${namespace}/${deployment_config}/resource_vars
     
-    memory_requests=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.strategy.resources.requests.memory}')
+    memory_requests=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.temaplete.spec.containers[].resources.requests.memory}')
     echo "export MEMORY_REQUESTS=${memory_requests}" >> ./${namespace}/${deployment_config}/resource_vars
 }
 
@@ -38,13 +38,13 @@ function main(){
             oc get dc -n $namespace --no-headers | awk '{print $1}' > /tmp/$namespace/deployment_configs
             while IFS= read -r tmp_deployment_config || [ -n "$tmp_deployment_config" ]
             do
-                echo "Current deployment ${tmp_deployment_config}"
+                echo "* Current deployment ${tmp_deployment_config}"
                 mkdir -p $namespace/$tmp_deployment_config
                 get_resource_limits $tmp_deployment_config $namespace
                 get_resource_requests $tmp_deployment_config $namespace
             done < /tmp/$namespace/deployment_configs
         else
-            echo "Current deployment ${deployment_config}"
+            echo "* Current deployment ${deployment_config}"
             mkdir -p $namespace/$deployment_config
             get_resource_limits $deployment_config $namespace
             get_resource_requests $deployment_config $namespace
