b49123513efc73b7b56e68aef659a003c971dcda
Ubiratan <r14.ubiratan.junior@gmail.com>
2022-04-29 16:49:15 -0300

    Correção do jsonpath

--- get_resources_limit_requests.sh
+++ get_resources_limit_requests.sh
@@ -5,10 +5,10 @@ function get_resource_limits(){
     local deployment_config="$1"
     local namespace="$2"
 
-    cpu_limits=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.temaplete.spec.containers[].resources.limits.cpu}')
+    cpu_limits=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.template.spec.containers[].resources.limits.cpu}')
     echo "export CPU_LIMITS=${cpu_limits}" > ./${namespace}/${deployment_config}/resource_vars
     
-    memory_limits=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.temaplete.spec.containers[].resources.limits.memory}')
+    memory_limits=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.template.spec.containers[].resources.limits.memory}')
     echo "export MEMORY_LIMITS=${memory_limits}" >> ./${namespace}/${deployment_config}/resource_vars
     
 }
@@ -17,10 +17,10 @@ function get_resource_requests(){
     local deployment_config=$1
     local namespace="$2"
 
-    cpu_requests=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.temaplete.spec.containers[].resources.requests.cpu}')
+    cpu_requests=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.template.spec.containers[].resources.requests.cpu}')
     echo "export CPU_REQUESTS=${cpu_requests}" >> ./${namespace}/${deployment_config}/resource_vars
     
-    memory_requests=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.temaplete.spec.containers[].resources.requests.memory}')
+    memory_requests=$(oc get dc/$deployment_config -n $namespace -o jsonpath='{.spec.template.spec.containers[].resources.requests.memory}')
     echo "export MEMORY_REQUESTS=${memory_requests}" >> ./${namespace}/${deployment_config}/resource_vars
 }
 
